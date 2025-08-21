a6340386d9590357794f531d14b0864f1ba48fef
RutwijGhadge <Vidyadham@123Society>
2025-08-21 08:26:54 +0530

    "Valid BST Tree Check"

---
+++ src/main/java/org/example/isValidBST_LeetCode_98/isValidBST.java
@@ -0,0 +1,27 @@
+package org.example.isValidBST_LeetCode_98;
+
+import org.example.Models.BuildTree;
+import org.example.Models.TreeNode;
+
+public class isValidBST {
+    public static boolean checkValidBST(TreeNode root,int l,int r){
+        if(root==null)
+            return true;
+
+        if(root.val>l && root.val<r){
+            return (checkValidBST(root.left,l,root.val) && checkValidBST(root.right,root.val,r));
+        }else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Integer[] input = {2, 1, 3};
+
+        TreeNode root = BuildTree.buildTree(input);
+       System.out.println(checkValidBST(root, Integer.MIN_VALUE,Integer.MAX_VALUE));
+    }
+}
+
+/*
+Important to Note : In the Leetcode DataTypes and the constraint
+ */
