25ae277de654029335ad41a6a8baedfd5aabca74
RutwijGhadge <rutwij0507.rg@gmail.com>
2025-09-04 09:34:42 +0530

    "Symmetric Tree Leetcode 101 "

---
+++ src/main/java/org/example/SymmetricBinaryTree_LeetCode_101/Symmetric.java
@@ -0,0 +1,36 @@
+package org.example.SymmetricBinaryTree_LeetCode_101;
+
+import org.example.Models.BuildTree;
+import org.example.Models.TreeNode;
+
+public class Symmetric {
+    public String isSymmetric(TreeNode root){
+        if(isSymmetricCheck(root.left,root.right))
+            return "Symmetric";
+        return "Not Symmetric";
+    }
+
+    public Boolean isSymmetricCheck(TreeNode A, TreeNode B){
+        if(A==null && B==null)
+            return true;
+
+        if(A!=null && B!=null && A.val==B.val){
+            return isSymmetricCheck(A.left,B.right) && isSymmetricCheck(A.right,B.left);
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        Integer[] input = {1,2,2,3,4,4,3};
+        TreeNode root = BuildTree.buildTree(input);
+        Symmetric symmetric=new Symmetric();
+        assert root != null;
+        System.out.println(symmetric.isSymmetric(root));
+    }
+}
+
+/*
+Note : If we want to compare the Nodes from the right and the Left tree at the Same Time as we did in this Question
+       you should pass left and right subtree to the recursive function and trat them as a separate tree.
+
+ */
