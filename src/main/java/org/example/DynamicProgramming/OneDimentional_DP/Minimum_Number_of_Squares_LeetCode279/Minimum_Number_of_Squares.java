47bda4c745a231ba6f7f70f48e3f2b3ebff77f95
RutwijGhadge <rutwij0507.rg@gmail.com>
2025-11-17 22:24:57 +0530

    " One Dimension DP Problems"

--- src/main/java/org/example/DynamicProgramming/OneDimentional_DP/Minimum_Number_of_Squares_LeetCode279/Minimum_Number_of_Squares.java
+++ src/main/java/org/example/DynamicProgramming/OneDimentional_DP/Minimum_Number_of_Squares_LeetCode279/Minimum_Number_of_Squares.java
@@ -6,28 +6,25 @@ public class Minimum_Number_of_Squares {
     int ans=Integer.MAX_VALUE;
     static int cnt=0;
     public int solution(int n,int[]dp){
-
+        if(n==0)
+            return 0;
         if(dp[n]!=-1)
             return dp[n];
 
+        int min_squares=Integer.MAX_VALUE;
         for(int i=1;i*i<=n;i++){
-            cnt++;
-            ans=Math.min(ans,solution(n-i*i,dp));
+            int nxt_square=solution(n-i*i,dp);
+            min_squares=Math.min(min_squares,nxt_square+1);
         }
-    
-        dp[n]=ans+1;
-        System.out.print(dp[n]+ " ");
-        return dp[n];
+        return dp[n]=min_squares;
     }
 
     public static void main(String[] args) {
         int n=12;
         int[]dp=new int[n+1];
         Arrays.fill(dp,-1);
-        dp[0]=0;
         Minimum_Number_of_Squares minimumNumberOfSquares=new Minimum_Number_of_Squares();
         System.out.println(minimumNumberOfSquares.solution(n,dp));
-        System.out.println("Total Number of Calls : "+cnt);
     }
 }
 
